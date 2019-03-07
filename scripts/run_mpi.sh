3ea5284065de13a3857609dbd6756ba3a27c3245
Ugirumurera <jugirumu@jugirum-32762s.nrel.gov>
2019-03-07 13:01:11 -0700

    changes to split chicago

--- scripts/run_mpi.sh
+++ scripts/run_mpi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $OTMMPIHOME/src/main/java
-mpijavac -d $OTMMPIHOME/out_mpijavac -cp $OTMSIMJAR:$OTMMPIHOME/lib/* metis/*.java xmlsplitter/*.java metagraph/*.java otm/*.java translator/*.java runner/RunnerMPI.java
+mpijavac -d $OTMMPIHOME/out_mpijavac -cp $OTMSIMJAR:$OTMMPIHOME/lib/* metis/*.java xmlsplitter/*.java metagraph/*.java otm/*.java translator/*.java runner/*.java
 cd $OTMMPIHOME/out_mpijavac
-mpirun -np 4 java -cp $OTMSIMJAR:$OTMMPIHOME/lib/*:. runner/RunnerMPI $OTMMPIHOME/test/50 2 1000 true
+mpirun -np 4 java -cp $OTMSIMJAR:$OTMMPIHOME/lib/*:. runner/RunnerMPI $OTMMPIHOME/test/50 1 2 1000 true
 #* 0 string : [prefix]
 #* 1 float : [sim_dt] sim dt in seconds
 #* 2 float : [duration] sim duration in seconds
