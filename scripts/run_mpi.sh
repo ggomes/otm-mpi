69ce9b2dd7adf391e4afc142799f715ec158f40c
Gabriel Gomes <gomes@gg-hp.attlocal.net>
2019-10-15 20:59:10 -0700

    run_mpi

--- scripts/run_mpi.sh
+++ scripts/run_mpi.sh
@@ -2,7 +2,7 @@
 cd $OTMMPIHOME/src/main/java
 mpijavac -d $OTMMPIHOME/out_mpijavac -cp $OTMSIMJAR:$OTMMPIHOME/lib/* metis/*.java xmlsplitter/*.java metagraph/*.java otm/*.java translator/*.java runner/*.java
 cd $OTMMPIHOME/out_mpijavac
-mpirun -np 4 java -cp $OTMSIMJAR:$OTMMPIHOME/lib/*:. runner/RunnerMPI $OTMMPIHOME/test/50 1 1000 true 10
+mpirun -np 2 java -cp $OTMSIMJAR:$OTMMPIHOME/lib/*:. runner/RunnerMPI $OTMMPIHOME/test/50 1 1000 true 10
 #* 0 string : [prefix]
 #* 1 int : [repetitions] 
 #* 2 float : [duration] sim duration in seconds
